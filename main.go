346f71a8cf0336441bdea3fc2bf25692c73c3c41
Papik Tamas <trapacska@gmail.com>
2018-08-23 11:06:12 +0200

    initial update

--- main.go
+++ main.go
@@ -2,45 +2,31 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"path/filepath"
 	"strings"
 
-	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/gobuffalo/envy"
 	"github.com/gorilla/mux"
 )
 
-const (
-	icnErr = "cross.svg"
-	icnOk  = "ok.svg"
-)
-
-type gittag struct {
-	Name   string `json:"name"`
-	Commit struct {
-		SHA string `json:"sha"`
-	} `json:"commit"`
-}
-
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/tag", tag).Methods("GET")
+	router.HandleFunc("/tag", tagHandler).Methods("GET")
+	router.HandleFunc("/update", updateHandler).Methods("GET")
 
 	if err := http.ListenAndServe(":"+envy.Get("PORT", "8000"), router); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func tag(w http.ResponseWriter, r *http.Request) {
+func tagHandler(w http.ResponseWriter, r *http.Request) {
 	setHeaders(w)
 
 	prURL := r.URL.Query().Get("pr")
-
 	if prURL == "" {
 		if err := respondWithIcon(icnErr, w); err != nil {
 			log.Fatal(err)
@@ -48,7 +34,7 @@ func tag(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	prDiff, err := getPRDiff(prURL)
+	prDiff, err := getPRDiffLines(prURL)
 	if err != nil {
 		log.Fatal(err)
 		if err := respondWithIcon(icnErr, w); err != nil {
@@ -75,12 +61,8 @@ func tag(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func checkGithubTag(giturl string, tag string, commit string) error {
-	giturl = strings.TrimSuffix(giturl, ".git")
-	giturl = strings.Replace(giturl, "https://github.com/", "https://api.github.com/repos/", 1)
-	giturl = giturl + "/tags"
-
-	r, err := http.Get(giturl)
+func updatePRs() error {
+	r, err := http.Get("https://api.github.com/repos/bitrise-io/bitrise-steplib/pulls?state=open")
 	if err != nil {
 		return err
 	}
@@ -90,77 +72,20 @@ func checkGithubTag(giturl string, tag string, commit string) error {
 		return err
 	}
 
-	var tags []gittag
-
-	if err := json.Unmarshal(b, &tags); err != nil {
+	var prs []githubpr
+	if err := json.Unmarshal(b, &prs); err != nil {
 		return err
 	}
 
-	for _, t := range tags {
-		if t.Name == tag && t.Commit.SHA == commit {
-			return nil
+	for _, pr := range prs {
+		if !strings.Contains(pr.Body, "![TagCheck](https://gogittag.herokuapp.com/tag?pr=") {
+			// update here
 		}
 	}
 
-	return fmt.Errorf("not found")
+	return nil
 }
 
-func getLinesAfterLineHasPrefix(s []string, prefix string) []string {
-	started := false
-	for _, line := range s {
-		if !started {
-			if strings.HasPrefix(line, prefix) {
-				started = true
-			}
-			s = s[1:]
-			continue
-		}
-	}
-	return s
-}
+func updateHandler(w http.ResponseWriter, r *http.Request) {
 
-func getLineContentAfterPrefix(s []string, prefix string) (f string) {
-	for _, line := range s {
-		if strings.HasPrefix(line, prefix) {
-			f = line
-			break
-		}
-	}
-	f = strings.TrimPrefix(f, prefix)
-	return
-}
-
-func trimLinesPrefixes(lns []string, s string) []string {
-	for i, l := range lns {
-		lns[i] = strings.TrimPrefix(l, s)
-	}
-	return lns
-}
-
-func getPRDiff(url string) ([]string, error) {
-	r, err := http.Get(url + ".diff")
-	if err != nil {
-		return nil, err
-	}
-
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return strings.Split(string(b), "\n"), nil
-}
-
-func setHeaders(w http.ResponseWriter) {
-	w.Header().Add("Content-Type", "image/svg+xml")
-	w.Header().Add("Cache-Control", "no-cache")
-}
-
-func respondWithIcon(icn string, w http.ResponseWriter) error {
-	b, err := fileutil.ReadBytesFromFile(icn)
-	if err != nil {
-		return err
-	}
-	_, err = w.Write(b)
-	return err
 }
---
+++ misc.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/bitrise-io/go-utils/fileutil"
+)
+
+const (
+	icnErr = "assets/cross.svg"
+	icnOk  = "assets/ok.svg"
+)
+
+type githubtag struct {
+	Name   string `json:"name"`
+	Commit struct {
+		SHA string `json:"sha"`
+	} `json:"commit"`
+}
+
+type githubpr struct {
+	URL     string `json:"url"`
+	DiffURL string `json:"diff_url"`
+	State   string `json:"state"`
+	Body    string `json:"body"`
+}
+
+func checkGithubTag(giturl string, tag string, commit string) error {
+	giturl = strings.TrimSuffix(giturl, ".git")
+	giturl = strings.Replace(giturl, "https://github.com/", "https://api.github.com/repos/", 1)
+	giturl = giturl + "/tags"
+
+	r, err := http.Get(giturl)
+	if err != nil {
+		return err
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+
+	var tags []githubtag
+
+	if err := json.Unmarshal(b, &tags); err != nil {
+		return err
+	}
+
+	for _, t := range tags {
+		if t.Name == tag && t.Commit.SHA == commit {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("not found")
+}
+
+func getLinesAfterLineHasPrefix(s []string, prefix string) []string {
+	started := false
+	for _, line := range s {
+		if !started {
+			if strings.HasPrefix(line, prefix) {
+				started = true
+			}
+			s = s[1:]
+			continue
+		}
+	}
+	return s
+}
+
+func getLineContentAfterPrefix(s []string, prefix string) (f string) {
+	for _, line := range s {
+		if strings.HasPrefix(line, prefix) {
+			f = line
+			break
+		}
+	}
+	f = strings.TrimPrefix(f, prefix)
+	return
+}
+
+func trimLinesPrefixes(lns []string, s string) []string {
+	for i, l := range lns {
+		lns[i] = strings.TrimPrefix(l, s)
+	}
+	return lns
+}
+
+func getPRDiffLines(url string) ([]string, error) {
+	r, err := http.Get(url + ".diff")
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(string(b), "\n"), nil
+}
+
+func setHeaders(w http.ResponseWriter) {
+	w.Header().Add("Content-Type", "image/svg+xml")
+	w.Header().Add("Cache-Control", "no-cache")
+}
+
+func respondWithIcon(icn string, w http.ResponseWriter) error {
+	b, err := fileutil.ReadBytesFromFile(icn)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(b)
+	return err
+}

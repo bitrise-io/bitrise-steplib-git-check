37d7e280b5a187d462b00f7293e6f5c30e60f1e0
Papik Tamas <trapacska@gmail.com>
2018-11-07 16:15:38 +0100

    added changelog posting and added release url to the first commit of steplib PRs

--- main.go
+++ main.go
@@ -32,14 +32,9 @@ func main() {
 }
 
 func tagHandler(w http.ResponseWriter, r *http.Request) {
-	////
-	// initializing handler
-	//
-
 	// setting image mime type and no-cache
 	setHeaders(w)
 
-	// parsing pr query from url
 	prID := r.URL.Query().Get("pr")
 	if prID == "" {
 		if err := respondWithIcon(icnErr, w); err != nil {
@@ -48,11 +43,7 @@ func tagHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//
-	////
-
-	// get the step.yml and the required infos from the pr
-	version, giturl, commit, err := parseStepYML(prID)
+	yml, version, err := parseStepYML(prID)
 	if err != nil {
 		if err := respondWithIcon(icnErr, w); err != nil {
 			fmt.Println(err)
@@ -60,13 +51,8 @@ func tagHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	////
-	// checking semver format
-	//
-
 	versionParts := strings.Split(version, ".")
 
-	// check if icon has 3 parts
 	if len(versionParts) != 3 {
 		if err := respondWithIcon(icnErrSemver, w); err != nil {
 			fmt.Println(err)
@@ -74,7 +60,6 @@ func tagHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// check if all 3 parts is castable to int
 	for _, part := range versionParts {
 		if _, err := strconv.Atoi(part); err != nil {
 			if err := respondWithIcon(icnErrSemver, w); err != nil {
@@ -84,40 +69,20 @@ func tagHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	//
-	////
-
-	////
-	// check git tag in the author's repo if it is not moved from the commit hash in the step.yml
-	//
-
-	if err := checkGithubTag(giturl, version, commit); err != nil {
+	if err := checkGithubTag(yml.Source.Git, version, yml.Source.Commit); err != nil {
 		if err := respondWithIcon(icnErrCommit, w); err != nil {
 			fmt.Println(err)
 		}
 		return
 	}
 
-	//
-	///
-
-	////
-	// everything ok
-	//
-
 	if err := respondWithIcon(icnOk, w); err != nil {
 		fmt.Println(err)
 	}
-
-	//
-	///
 }
 
-// webhook wired in the github repo
 func updateHandler(w http.ResponseWriter, r *http.Request) {
-	////
-	// handle only pull request
-	//
+
 	if r.Header.Get("X-Github-Event") != "pull_request" {
 		return
 	}
@@ -128,64 +93,86 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if pr.Action != "opened" {
-		return
+	if pr.Action == "opened" {
+		exists, err := isPRHasStepYML(fmt.Sprintf("%d", pr.Number))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if !exists {
+			return
+		}
+
+		if strings.Contains(pr.PullRequest.Body, fmt.Sprintf("https://%s/tag?pr=%d", hostBaseURL, pr.Number)) {
+			return
+		}
+
+		yml, version, err := parseStepYML(fmt.Sprintf("%d", pr.Number))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		apiURL := fmt.Sprintf("https://api.github.com/repos/bitrise-io/bitrise-steplib/pulls/%d", pr.Number)
+		badgeContent := fmt.Sprintf("![TagCheck](https://%s/tag?pr=%d)\r\n\r\n", hostBaseURL, pr.Number)
+		releaseURLContent := ""
+
+		if strings.Contains(yml.Source.Git, "/bitrise-io/") || strings.Contains(yml.Source.Git, "/bitrise-steplib/") {
+			releaseURLContent = fmt.Sprintf("%s/releases/%s\r\n\r\n", strings.TrimSuffix(yml.Source.Git, ".git"), version)
+		}
+
+		newBody := map[string]interface{}{
+			"body": badgeContent + releaseURLContent + pr.PullRequest.Body,
+		}
+
+		b, err := json.Marshal(newBody)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		c := http.Client{}
+		req, err := http.NewRequest("PATCH", apiURL, bytes.NewReader(b))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		req.SetBasicAuth(os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_ACCESS_TOKEN"))
+		_, err = c.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 
-	//
-	////
+	if pr.Action == "closed" && pr.PullRequest.Merged {
+		yml, version, err := parseStepYML(fmt.Sprintf("%d", pr.Number))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-	////
-	// check if the pr just opened has step.yml in it
-	//
-	exists, err := isPRHasStepYML(fmt.Sprintf("%d", pr.Number))
-	if err != nil {
-		fmt.Println(err)
-		return
+		if strings.Contains(yml.Source.Git, "/bitrise-io/") || strings.Contains(yml.Source.Git, "/bitrise-steplib/") {
+			if yml.Title == nil {
+				return
+			}
+
+			title := *yml.Title + " v" + version
+			body, err := loadReleaseBody(yml.Source.Git, version)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// append git release url
+			body += "\n\n" + fmt.Sprintf("%s/releases/%s\r\n\r\n", strings.TrimSuffix(yml.Source.Git, ".git"), version)
+
+			if err := createDiscourseTopic(title, body); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
 	}
-
-	if !exists {
-		return
-	}
-
-	//
-	////
-	if strings.Contains(pr.PullRequest.Body, fmt.Sprintf("https://%s/tag?pr=%d", hostBaseURL, pr.Number)) {
-		return
-	}
-
-	////
-	// updating the PR's initial comment section: append badge as first element
-	//
-
-	apiURL := fmt.Sprintf("https://api.github.com/repos/bitrise-io/bitrise-steplib/pulls/%d", pr.Number)
-	badgeContent := fmt.Sprintf("![TagCheck](https://%s/tag?pr=%d)\r\n\r\n", hostBaseURL, pr.Number)
-	newBody := map[string]interface{}{
-		"body": badgeContent + pr.PullRequest.Body,
-	}
-
-	// convert new body message to json
-	b, err := json.Marshal(newBody)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// call authenticated PATCH request
-	c := http.Client{}
-	req, err := http.NewRequest("PATCH", apiURL, bytes.NewReader(b))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	req.SetBasicAuth(os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_ACCESS_TOKEN"))
-	_, err = c.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	//
-	////
 }

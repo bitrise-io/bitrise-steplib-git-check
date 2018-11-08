b8544b55fe6afc7f176b56cad4a60a00c4e5e0af
Tamas Papik <trapacska@gmail.com>
2018-11-08 09:44:17 +0100

    added changelog posting and release url to the first commit of SL PRs
    * added changelog posting and added release url to the first commit of steplib PRs
    
    * added community org
    
    * formatted changelog url

--- misc.go
+++ misc.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -30,6 +32,10 @@ type githubtag struct {
 	} `json:"commit"`
 }
 
+type githubrelease struct {
+	Body string `json:"body"`
+}
+
 type pullRequestModel struct {
 	Action      string  `json:"action"`
 	Number      int     `json:"number"`
@@ -37,6 +43,7 @@ type pullRequestModel struct {
 }
 
 type content struct {
+	Merged bool   `json:"merged"`
 	Number int    `json:"number"`
 	Body   string `json:"body"`
 }
@@ -64,6 +71,20 @@ func checkGithubTag(giturl string, tag string, commit string) error {
 	}
 
 	return fmt.Errorf("not found")
+}
+
+func loadReleaseBody(giturl string, tag string) (string, error) {
+	giturl = strings.TrimSuffix(giturl, ".git")
+	giturl = strings.Replace(giturl, "https://github.com/", "https://api.github.com/repos/", 1)
+	giturl = giturl + "/releases/tags/" + tag
+
+	var release githubrelease
+
+	if err := httpLoadJSON(giturl, &release); err != nil {
+		return "", err
+	}
+
+	return release.Body, nil
 }
 
 func setHeaders(w http.ResponseWriter) {
@@ -124,30 +145,62 @@ func isPRHasStepYML(prID string) (bool, error) {
 	return false, nil
 }
 
-func parseStepYML(prID string) (version string, url string, commit string, err error) {
+func parseStepYML(prID string) (stepmanModels.StepModel, string, error) {
 	var files []file
 	if err := httpLoadJSON(fmt.Sprintf("https://api.github.com/repos/bitrise-io/bitrise-steplib/pulls/%s/files", prID), &files); err != nil {
-		return "", "", "", err
+		return stepmanModels.StepModel{}, "", err
 	}
 
 	for _, file := range files {
 		if strings.HasSuffix(file.Filename, "step.yml") && strings.HasPrefix(file.Filename, "steps/") {
 			var yml stepmanModels.StepModel
 			if err := httpLoadYML(file.RawURL, &yml); err != nil {
-				return "", "", "", err
+				return stepmanModels.StepModel{}, "", err
 			}
 
 			if yml.Source == nil {
-				return "", "", "", fmt.Errorf("no source in step.yml")
+				return stepmanModels.StepModel{}, "", fmt.Errorf("no source in step.yml")
 			}
 
-			version = filepath.Base(filepath.Dir(file.Filename))
-			url = yml.Source.Git
-			commit = yml.Source.Commit
-
-			return version, url, commit, nil
+			return yml, filepath.Base(filepath.Dir(file.Filename)), nil
 		}
 	}
 
-	return "", "", "", fmt.Errorf("no step.yml found")
+	return stepmanModels.StepModel{}, "", fmt.Errorf("no step.yml found")
+}
+
+func createDiscourseTopic(title, body string) error {
+	apiKey := os.Getenv("DISCOURSE_API_KEY")
+	if apiKey == "" {
+		return fmt.Errorf("DISCOURSE_API_KEY is not set")
+	}
+	userName := os.Getenv("DISCOURSE_API_USERNAME")
+	if userName == "" {
+		return fmt.Errorf("DISCOURSE_API_USERNAME is not set")
+	}
+	category := os.Getenv("DISCOURSE_CATEGORY")
+	if category == "" {
+		return fmt.Errorf("DISCOURSE_CATEGORY is not set")
+	}
+	discourseURL := os.Getenv("DISCOURSE_URL")
+	if discourseURL == "" {
+		return fmt.Errorf("DISCOURSE_URL is not set")
+	}
+
+	formData := url.Values{}
+	formData.Set("api_key", apiKey)
+	formData.Set("api_username", userName)
+	formData.Set("raw", body)
+	formData.Set("category", category)
+	formData.Set("title", title)
+
+	resp, err := http.PostForm(discourseURL+"/posts.json", formData)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return fmt.Errorf("Invalid response code: %d from: %s", resp.StatusCode, discourseURL+"/posts.json")
+	}
+
+	return nil
 }

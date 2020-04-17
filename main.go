52f9936f889a493f6d388bbbe3d2f78c7942b2d6
Tamas Papik <trapacska@gmail.com>
2020-04-17 11:56:58 +0200

    added notification for new steps (#3)

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func tagHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	yml, version, err := parseStepYML(prID)
+	yml, version, _, err := parseStep(prID)
 	if err != nil {
 		if err := respondWithIcon(icnErr, w); err != nil {
 			fmt.Println(err)
@@ -81,6 +81,15 @@ func tagHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func isNewStep(stepID string) (bool, error) {
+	resp, err := http.Get("https://api.github.com/repos/bitrise-io/bitrise-steplib/contents/steps/" + stepID)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusNotFound, nil
+}
+
 func updateHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Header.Get("X-Github-Event") != "pull_request" {
@@ -108,22 +117,34 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		stepDefinition, version, err := parseStepYML(fmt.Sprintf("%d", pr.Number))
+		stepDefinition, version, stepID, err := parseStep(fmt.Sprintf("%d", pr.Number))
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
+		newStep, err := isNewStep(stepID)
+		if err != nil {
+			fmt.Println("unable to check if", stepID, "is a new step, error:", err)
+			return
+		}
+
 		apiURL := fmt.Sprintf("https://api.github.com/repos/bitrise-io/bitrise-steplib/pulls/%d", pr.Number)
 		badgeContent := fmt.Sprintf("![TagCheck](https://%s/tag?pr=%d)\r\n\r\n", hostBaseURL, pr.Number)
-		releaseURLContent := ""
 
+		releaseURLContent := ""
 		if strings.Contains(stepDefinition.Source.Git, "/bitrise-io/") || strings.Contains(stepDefinition.Source.Git, "/bitrise-steplib/") || strings.Contains(stepDefinition.Source.Git, "/bitrise-community/") {
 			releaseURLContent = fmt.Sprintf("%s/releases/%s\r\n\r\n", strings.TrimSuffix(stepDefinition.Source.Git, ".git"), version)
 		}
 
+		notifications := ""
+		if newStep {
+			notifications = "\r\n\r\n"
+			notifications += "**New Step**\r\nThank you for the new Step share! The CI check might will fail due to our extended validation engine. Nothing to worry about yet, we will get back to you shortly."
+		}
+
 		newBody := map[string]interface{}{
-			"body": badgeContent + releaseURLContent + pr.PullRequest.Body,
+			"body": badgeContent + releaseURLContent + pr.PullRequest.Body + notifications,
 		}
 
 		b, err := json.Marshal(newBody)
@@ -148,7 +169,7 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if pr.Action == "closed" && pr.PullRequest.Merged {
-		stepDefinition, version, err := parseStepYML(fmt.Sprintf("%d", pr.Number))
+		stepDefinition, version, _, err := parseStep(fmt.Sprintf("%d", pr.Number))
 		if err != nil {
 			fmt.Println(err)
 			return
